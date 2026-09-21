3ae02b028a3b856767177b4ce5114690b949edcd
bahrululumpdmkabpkl <sverenicen@gmail.com>
2026-09-21 12:48:11 +0700

    Update Supabase anon key in configuration
    Updated the anon key in the Supabase configuration.

--- supabase-config.js
+++ supabase-config.js
@@ -2,5 +2,5 @@
 // Gunakan anon key saja. Jangan masukkan service_role key ke file ini.
 window.SUPABASE_CONFIG = {
   url: 'https://joebnfvbktmmzimnjnpd.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZXMiLCJyZWYiOiJqb2VibmZ2Ymt0bW16aW1uam5wZCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg5OTYxNTE4LCJleHAiOjIxMDU1Mzc1MTh9.jlqa8aCuYRPmy6vxjapXSlZAnN4aNHIjbfbMkfh-pRg'
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpvZWJuZnZia3RtbXppbW5qbnBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk5NjE1MTgsImV4cCI6MjEwNTUzNzUxOH0.jlqa8aCuYRPmy6vxjapXSlZAnN4aNHIjbfbMkfh-pRg'
 };
