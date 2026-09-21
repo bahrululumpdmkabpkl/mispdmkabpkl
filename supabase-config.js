d4c8c571e6a7d1a3ada908bb5bc97c493c047402
bahrululumpdmkabpkl <sverenicen@gmail.com>
2026-09-21 12:47:29 +0700

    Update Supabase configuration with project details

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Isi dengan nilai dari Supabase Project Settings > API.
 // Gunakan anon key saja. Jangan masukkan service_role key ke file ini.
 window.SUPABASE_CONFIG = {
-  url: '',
-  anonKey: ''
+  url: 'https://joebnfvbktmmzimnjnpd.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZXMiLCJyZWYiOiJqb2VibmZ2Ymt0bW16aW1uam5wZCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzg5OTYxNTE4LCJleHAiOjIxMDU1Mzc1MTh9.jlqa8aCuYRPmy6vxjapXSlZAnN4aNHIjbfbMkfh-pRg'
 };
